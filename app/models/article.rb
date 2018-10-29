2ad6f958390b0ab9aaf14fbbe07731e38bf73134
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-10-28 20:24:04 -0600

    add relationships

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
 
   validates_presence_of :title, :body
 
+  has_many :comments
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
+  describe "validations" do
+    it {should belong_to(:article)}
+  end
 
-  it {should belong_to(:article)}
-
+  describe "relationships" do
+    it {should belong_to :article}
+  end
 end
